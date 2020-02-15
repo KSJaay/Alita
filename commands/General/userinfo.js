58f7ba5b9bb814380312295e7ba6d27b40d0b589
KSJaay1 <ksjaay@gmail.com>
2020-02-15 23:56:59 +0000

    Added server and user info

--- commands/General/userinfo.js
+++ commands/General/userinfo.js
@@ -21,12 +21,51 @@ class Userinfo extends Command {
 
     async run (message, args, db) {
 
+      let user = message.author;
+      let userID = user.id;
+      let userName = user.tag;
+      let joinedDate = user.createdAt;
+      let userStatus = user.presence.status;
+
+      let member = null;
+      if(message.guild){
+          member = await message.guild.members.fetch(user).catch((err) => {});
+      }
+
+      let memberColor = member.displayHexColor;
+      let memberHRole = member.roles.highest;
+      let memberJoined = member.joinedAt;
+      console.log(member)
+
       let announcementEmbed = new Discord.MessageEmbed()
-      .setAuthor(message.author.username)
+      .setAuthor(user.username, user.displayAvatarURL())
+      .setThumbnail(user.displayAvatarURL())
       .setFooter(db.config.embed.footer)
       .setColor(db.config.embed.color)
+      .addField("ID", userID, true)
+      .addField("Username", userName,true)
+      .addField("Created Discord", `${joinedDate.toLocaleDateString()}`)
+      .addField("Joined server", `${memberJoined.toLocaleDateString()}`)
+      .addField("Member color", memberColor, true)
+      .addField("Highest role", member.roles.highest, true)
+      .addField("Total roles", member.roles.cache.size)
+
+
+      if(userStatus === "dnd"){
+        announcementEmbed.addField("Staus", ":red_circle: Do not disturb")
+      }else if(userStatus === "idle"){
+        announcementEmbed.addField("Staus", ":yellow_circle: Idle")
+      }else if(userStatus === "online"){
+        announcementEmbed.addField("Staus", ":green_circle: Online")
+      }else if(userStatus === "offline"){
+        announcementEmbed.addField("Staus", ":white_circle: Offline")
+      }else{
+        announcementEmbed.addField("Staus", "IDK")
+      }
+
       return message.channel.send(announcementEmbed)
     }
-}
+  }
+
 
 module.exports = Userinfo;
