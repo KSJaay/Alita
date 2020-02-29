45446a2ac7a3e9f80745800dc78b18af8907fba1
KSJaay1 <ksjaay@gmail.com>
2020-02-29 02:23:34 +0000

    Add warn command

--- base/Member.js
+++ base/Member.js
@@ -5,6 +5,6 @@ module.exports = mongoose.model("Member", new mongoose.Schema({
     id: { type: String },
     guildID: { type: String },
     registeredAt: { type: Number, default: Date.now() },
-
+    warnings: { type: Array, default: [] }
 
 }));
