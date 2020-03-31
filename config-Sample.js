d45d787266c1fcab502779d68c8a2fb0810d8682
KSJaay1 <ksjaay@gmail.com>
2020-03-31 16:37:15 +0100

    Rewriting the whole bot

--- config-Sample.js
+++ config-Sample.js
@@ -3,17 +3,8 @@ module.exports = {
 	token: "BOT TOKEN",
 	mongoIP: "mongodb://localhost:27017/AlitaBot",
 	prefix: "-a",
-	embed: {
-		color: "#7122E3",
-		win: "#00FF00",
-		lost: "#FF0000",
-		footer: "Alitabot.me/premium ❤️"
-	},
 	owner: {
 		id: "249955383001481216",
 		name: "KSJaay#2487"
 	},
-	api: {
-		trackergg:""
-	}
 }
