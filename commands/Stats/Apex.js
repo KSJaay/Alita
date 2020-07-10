1b142300ddce2ce6c5fe56d73dcb887cfd67ab7a
KSJaay1 <ksjaay@gmail.com>
2020-07-10 01:05:13 +0100

    Updated to V2 API

--- commands/Stats/Apex.js
+++ commands/Stats/Apex.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const config = require("../../config.json");
-const ApexTab = require("apexlegendsjs");
+const fetch = require("node-fetch");
 module.exports = {
       //Command Information
       name: "apex",
@@ -22,66 +22,49 @@ module.exports = {
                 return message.channel.send("Unable to find command");
             }
 
-            //Create a new ApexTab using the API key
-            let apexStats = new ApexTab(config.trackergg);
-
-
-
             //Get the platform the user entered
-            let platform = args[0] ? args[0].toUpperCase() : "PC";
+            let platform = args[0] ? args[0].toLowerCase() : "origin";
 
             //If the platform isn't the following then return error message
-            if((platform != "PC" && platform != "XBOX" && platform != "PSN")){
-                return message.channel.send("Please mention a valid platform (PC, XBOX or PSN)");
+            if((platform != "origin" && platform != "xbl" && platform != "psn")){
+                return message.channel.send("Please mention a valid platform (Origin, xbl or PSN)");
             }
 
             //Get the username
             let user = args.slice(1).join(" ");
+
             //If there isn't a username return error
             if(!user){
                 return message.channel.send("Please mention a valid username");
             }
 
+            const headers = { headers: { 'TRN-Api-Key': config.trackergg } };
 
-            //Get the users data using the username and platform
-            apexStats.getDetailedPlayer(user, platform)
-              .then((dataPulled)=>{
-                  let level = dataPulled.metadata.level;
-                  let rankName = dataPulled.metadata.rankName;
-                  let rankImage = dataPulled.metadata.rankImage;
-                  let playerImage = dataPulled.metadata.avatarUrl;
+            fetch(`https://public-api.tracker.gg/v2/apex/standard/profile/${platform}/${user}`, headers)
+            .then(res => res.json())
+            .then(json => {
+              if(json.errors){
+                return message.channel.send("Uh-Oh! Looks like we couldn't find your profile. Please make sure the platform and Username are correct.")
+              }
 
-                    // Add the userdata to an embed
-                    let embed = new Discord.MessageEmbed()
-                    .setAuthor(`Apex data for ${dataPulled.metadata.platformUserHandle} on ${platform}`, playerImage)
-                    .setThumbnail(rankImage)
-                    .addFields(
-                    { name: `Rank`, value: rankName},
-                    { name: `Level`, value: level}
-                  )
+              let embed = new Discord.MessageEmbed()
+              .setAuthor(`Apex Stats ${json.data.platformInfo.platformUserId}`, json.data.platformInfo.avatarUrl)
 
-                  let i = 0;
-                  let arrSize = dataPulled.stats;
+              for(let i = 0; i < json.data.segments.length; i++){
+                let objName = Object.getOwnPropertyNames(json.data.segments[i].stats)
+                var desc = "";
+                for(let j = 0; j < objName.length; j++){
+                  desc += (json.data.segments[i].stats[objName[j]].displayName + ": " + json.data.segments[i].stats[objName[j]].displayValue + "\n");
+                }
+                embed.addField(json.data.segments[i].metadata.name, desc)
+              }
+              embed.setColor(config.color)
+              .setFooter(config.footer);
 
-                    for (i = 0; i < arrSize.length; i++) {
-                      if(dataPulled.stats[i].value <= 0){
+              return message.channel.send(embed)
 
-                      }else if(dataPulled.stats[i].value >= 1){
+            });
 
-                      embed.addFields({ name: dataPulled.stats[i].metadata.name, value: dataPulled.stats[i].value})
-                      }
-                    }
-                    embed.setColor(config.color)
-                    .setFooter(config.footer);
-                    //send the embed
-                     return message.channel.send(embed);
-
-
-              })
-              .catch((err)=>{
-                  console.log(err)
-                  return message.channel.send("ERROR! Unable to find this user. Please try again")
-            })
 
 
     },
