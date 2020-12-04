ea709369fa938c924919699322c3bc4eb48db87e
KSJaay1 <ksjaay@gmail.com>
2020-12-04 01:59:24 +0000

    Remove useless stuff from dog command

--- commands/Images/Dog.js
+++ commands/Images/Dog.js
@@ -17,13 +17,8 @@ module.exports = {
 
     async execute(client, message, args, data) {
 
-        // Check for mentioned users
-        // If no member is mentioned then use message author else find member from guild
-        let user = !args[0] ? message.author : await findMember(args[0], message.guild)
-
         let url = await randomPuppy();
 
-        // Create embed and send user the avatar with links
         let embed = new Discord.MessageEmbed()
         .setTitle(`Doggo :heart_eyes:`)
         .setImage(url)
