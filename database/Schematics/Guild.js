d92a55d822f41367e9d689b2bc3c39d8bbe91bac
KSJaay1 <ksjaay@gmail.com>
2020-12-13 02:05:41 +0000

    Added addons to guild Document

--- database/Schematics/Guild.js
+++ database/Schematics/Guild.js
@@ -7,4 +7,21 @@ module.exports = mongoose.model("Guild", new mongoose.Schema({
   registeredAt: { type: Number, default: Date.now() },
   prefix: { type: String, default: config.prefix },
 
+  addons: { type: Object, default: { // Extra features data
+    welcome: {
+      enabled: false, // Welcome features are enabled
+      channel:  null, // ID for the channel to send messages to
+      message: null, // Custom message
+      image: false, // Check if image is enabled
+      embed: false // Check if embed is enabled
+    },
+    goodbye: {
+      enabled: false, // Goodbye features are enabled
+      channel:  null, // ID for channel to send messages to
+      message: null, // Custom message
+      image: false, // Check if image is enabled
+      embed: false // Check if embed is enabled
+    }
+  }}
+
 }));
