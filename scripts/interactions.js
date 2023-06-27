b8cbd746f6bc521e740ffe11b675a8162982bedc
ksjaay <ksjaay@gmail.com>
2023-06-27 23:04:06 +0100

    Adds scripts to upload slash commands to global and guild specific

---
+++ scripts/interactions.js
@@ -0,0 +1,33 @@
+// Upload all the interactions to discord
+
+const fs = require("fs");
+
+const {REST} = require("@discordjs/rest");
+const {Routes} = require("discord-api-types/v9");
+
+const commands = [];
+const commandFolders = fs.readdirSync(__dirname + "/commands");
+
+for (const folder of commandFolders) {
+  const commandFiles = fs
+    .readdirSync(__dirname + `/commands/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const command = require(__dirname + `/commands/${folder}/${file}`);
+    commands.push(command.interaction);
+  }
+}
+
+const rest = new REST({version: "9"}).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), {
+      body: commands,
+    });
+
+    console.log("Successfully registered application commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
