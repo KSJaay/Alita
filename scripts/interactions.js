5241035c86c5011f10b8a9eedd3a2621b88778df
ksjaay <ksjaay@gmail.com>
2023-07-04 00:53:50 +0100

    Fixes issues with global interaction commands

--- scripts/interactions.js
+++ scripts/interactions.js
@@ -1,19 +1,24 @@
 // Upload all the interactions to discord
 
 const fs = require("fs");
+const path = require("path");
+const dotenv = require("dotenv");
 
 const {REST} = require("@discordjs/rest");
 const {Routes} = require("discord-api-types/v9");
 
+dotenv.config();
+
 const commands = [];
-const commandFolders = fs.readdirSync(__dirname + "/commands");
+const commandPath = path.join(__dirname, "..", "/commands");
+const commandFolders = fs.readdirSync(commandPath);
 
 for (const folder of commandFolders) {
   const commandFiles = fs
-    .readdirSync(__dirname + `/commands/${folder}`)
+    .readdirSync(commandPath + `/${folder}`)
     .filter((file) => file.endsWith(".js"));
   for (const file of commandFiles) {
-    const command = require(__dirname + `/commands/${folder}/${file}`);
+    const command = require(commandPath + `/${folder}/${file}`);
     commands.push(command.interaction);
   }
 }
